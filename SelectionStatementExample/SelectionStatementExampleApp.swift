b78038e52e5d150c64b01a5f610110b0ea589813
Noah Soubliere <nsoubliere@lcs.on.ca>
2021-12-02 09:08:52 -0500

    Initial Commit

---
+++ SelectionStatementExample/SelectionStatementExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SelectionStatementExampleApp.swift
+//  SelectionStatementExample
+//
+//  Created by Noah Alexandre Soubliere on 2021-12-02.
+//
+
+import SwiftUI
+
+@main
+struct SelectionStatementExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
